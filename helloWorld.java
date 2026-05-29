9361257eacccc796a074cd17ec91ea408551c5a2
komal2289 <donakarikomal@gmail.com>
2026-05-29 14:34:41 +0530

    helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld
   public static void main(String[] args)
   {
     system.out.println("HelloWorld");
-    system.out.println("Java file");
+    system.out.print("This is a Java file");
+    system.out.print(".....");
   }
 }
